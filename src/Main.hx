553655e55ba72dda3c16ecceea27e6bc49418b21
Matt Swieboda <mswieboda@users.noreply.github.com>
2021-08-24 19:39:17 -0500

    simpifies world example to prepare to move tree around

--- src/Main.hx
+++ src/Main.hx
@@ -1,10 +1,11 @@
 import hxd.Key in K;
 
 class WorldMesh extends h3d.scene.World {
-  override function initChunkSoil(c:h3d.scene.World.WorldChunk) {
+  override function initChunkSoil(c: h3d.scene.World.WorldChunk) {
     var cube = new h3d.prim.Cube(chunkSize, chunkSize, 0);
     cube.addNormals();
     cube.addUVs();
+
     var soil = new h3d.scene.Mesh(cube, c.root);
     soil.x = c.x;
     soil.y = c.y;
@@ -21,51 +22,43 @@ class Main extends hxd.App {
 
   override function init() {
     world = new WorldMesh(16, 128, s3d);
-    var t = world.loadModel(hxd.Res.tree);
-    var r = world.loadModel(hxd.Res.rock);
 
-    for(i in 0...1000) {
+    var treeModel = world.loadModel(hxd.Res.tree);
+    var rockModel = world.loadModel(hxd.Res.rock);
+
+    // add 300 rocks
+    for(i in 0...300) {
       world.add(
-        Std.random(2) == 0 ? t : r,
-        Math.random() * 128, Math.random() * 128,
+        rockModel,
+        Math.random() * 128,
+        Math.random() * 128,
         0,
         1.2 + hxd.Math.srand(0.4),
         hxd.Math.srand(Math.PI)
       );
     }
 
+    // add tree in middle, to move around like a character
+    world.add(treeModel, 64, 64, 0, 1.2, 0);
+
     world.done();
 
+    // add directional light
     new h3d.scene.fwd.DirLight(new h3d.Vector( 0.3, -0.4, -0.9), s3d);
-    cast(s3d.lightSystem,h3d.scene.fwd.LightSystem).ambientLight.setColor(0x909090);
+    cast(s3d.lightSystem, h3d.scene.fwd.LightSystem).ambientLight.setColor(0x909090);
 
-    s3d.camera.target.set(72, 72, 0);
-    s3d.camera.pos.set(120, 120, 40);
-
-    shadow = s3d.renderer.getPass(h3d.pass.DefaultShadowMap);
-    shadow.size = 2048;
-    shadow.power = 200;
-    shadow.blur.radius= 0;
-    shadow.bias *= 0.1;
-    shadow.color.set(0.7, 0.7, 0.7);
-
-    var parts = new h3d.parts.GpuParticles(world);
-    var g = parts.addGroup();
-    g.size = 0.2;
-    g.gravity = 1;
-    g.life = 10;
-    g.nparts = 10000;
-    g.emitMode = CameraBounds;
-    parts.volumeBounds = h3d.col.Bounds.fromValues( -20, -20, 15, 40, 40, 40);
-
+    // set camera
+    s3d.camera.target.set(64, 64, 0);
+    s3d.camera.pos.set(128, 128, 64);
     s3d.camera.zNear = 1;
     s3d.camera.zFar = 100;
     new h3d.scene.CameraController(s3d).loadFromCamera();
 
+    // add debug text/HUD
     tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
   }
 
-  override function update(dt:Float) {
+  override function update(dt: Float) {
     tf.text = ""+engine.drawCalls;
   }
 
